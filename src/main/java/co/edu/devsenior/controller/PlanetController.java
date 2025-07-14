27a46c6fc061a6560e0d9c515c9e6632a74c3976
DJAngel973 <angel.djuancho@gmail.com>
2025-07-14 00:29:20 -0500

    refactor(PlanetControl): MVC is applied, a controller class is implemented to take the view and interact with the models

---
+++ src/main/java/co/edu/devsenior/controller/PlanetController.java
@@ -0,0 +1,52 @@
+package main.java.co.edu.devsenior.controller;
+
+import main.java.co.edu.devsenior.model.Planet;
+import main.java.co.edu.devsenior.model.PlanetSystem;
+import main.java.co.edu.devsenior.view.ConsoleUI;
+
+public class PlanetController {
+
+    // Attributes
+    private final PlanetSystem planetSystem;
+    private final ConsoleUI ui;
+
+    // Constructor
+   public PlanetController(){
+       this.planetSystem = new PlanetSystem();
+       this.ui = new ConsoleUI();
+   }
+
+   // Method to start
+    public void start(){
+       boolean exit = false;
+       int option;
+
+       do{
+           ui.showMenu(); // Called at method for show menu primary
+           option = ui.readOptionMenu(); // Called at method for take option user
+
+           switch (option){
+               case 1 -> requestDestination();
+               case 2 -> {}
+               case 3 -> {}
+               case 4 -> {
+                   exit = true;
+                   ui.exit();
+               }
+               default -> ui.showError("Opción no valida");
+           }
+       }while (!exit);
+    }
+
+    // Request destination for trip
+    public void requestDestination(){
+
+       String namePlanet = ui.readNamePlanet();
+       Planet planet = planetSystem.searchPlanet(namePlanet);
+       if (planet != null) {
+           ui.showPlanet(planet);
+       }else{
+           ui.showError("El planeta ingresado no existe.");
+       }
+    }
+}
