3733338794f55a572c9afa136d0c74ac1e98011d
DJAngel973 <angel.djuancho@gmail.com>
2025-07-14 00:31:33 -0500

    refactor(Mian): MVC is applied, the main class is implemented to initialize the project

---
+++ src/main/java/co/edu/devsenior/Main.java
@@ -0,0 +1,10 @@
+package main.java.co.edu.devsenior;
+
+import main.java.co.edu.devsenior.controller.PlanetController;
+
+public class Main {
+    public static void main(String[] args) {
+        PlanetController controller = new PlanetController();
+        controller.start();
+    }
+}
