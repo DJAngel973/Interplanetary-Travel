fb1d4263ae4a3dee7679f820b6f09c53d7db9aa6
DJAngel973 <angel.djuancho@gmail.com>
2025-07-18 00:21:40 -0500

    feat(SpacecraftService): create a Service class for the 'spacecraft' logic

--- src/main/java/co/edu/devsenior/model/SpacecraftService.java
+++ src/main/java/co/edu/devsenior/model/SpacecraftService.java
@@ -1,4 +1,18 @@
 package main.java.co.edu.devsenior.model;
 
 public class SpacecraftService {
+
+    // Method for entering passengers on the trip
+    public String assignPassengers(Spacecraft spacecraft, int numberPeople){
+
+        if (numberPeople <= 0) {
+            return "El número de personas debe ser mayor a cero.";
+        }else if (numberPeople > spacecraft.getPeopleMax()){
+            return "El número de personas (" + numberPeople + ") es mayor a la capacidad de la nave (" + spacecraft.getPeopleMax() +").";
+        }
+
+        // Save entered amount
+        spacecraft.setPeople(numberPeople);
+        return null;
+    }
 }
