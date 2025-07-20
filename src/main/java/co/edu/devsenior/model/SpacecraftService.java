8bca30f545d28bf18513c84ea6781ad9e13b985f
DJAngel973 <angel.djuancho@gmail.com>
2025-07-20 16:08:10 -0500

    feat(SpacecraftService): the 'while' is modified, improves in error message and correct the 'null' he showed in execution

--- src/main/java/co/edu/devsenior/model/SpacecraftService.java
+++ src/main/java/co/edu/devsenior/model/SpacecraftService.java
@@ -1,18 +1,27 @@
 package main.java.co.edu.devsenior.model;
 
+import main.java.co.edu.devsenior.view.ConsoleUI;
+
 public class SpacecraftService {
+
+    private final ConsoleUI consoleUI = new ConsoleUI();
 
     // Method for entering passengers on the trip
     public String assignPassengers(Spacecraft spacecraft, int numberPeople){
 
-        if (numberPeople <= 0) {
-            return "El número de personas debe ser mayor a cero.";
-        }else if (numberPeople > spacecraft.getPeopleMax()){
-            return "El número de personas (" + numberPeople + ") es mayor a la capacidad de la nave (" + spacecraft.getPeopleMax() +").";
-        }
+        while (numberPeople <= 0 || numberPeople > spacecraft.getPeopleMax()) {
 
-        // Save entered amount
+            // Show error
+            if (numberPeople <= 0) {
+                System.out.println("Error: el número debe ser mayor que cero.");
+            } else {
+                System.out.println("Error: el número excede la cantidad de la nave (" + spacecraft.getPeopleMax() + ").");
+            }
+            // We request the number again
+            numberPeople = consoleUI.errorPeople();
+        }
+        // Save number of people
         spacecraft.setPeople(numberPeople);
-        return null;
+        return String.format("Pasajeros ingresados (%d) a la nave %s ", spacecraft.getPeople(), spacecraft.getName());
     }
 }
